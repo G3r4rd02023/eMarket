6fd8c187f373a00eeaf1c61cf06ad7e8063460a8
gerardo lanza <glanza007@gmail.com>
2021-02-01 11:46:57 -0600

    Countries

---
+++ EMarket/Data/DataContext/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using EMarket.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMarket.Data.DataContext
+{
+    public class DataContext :DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>()
+            .HasIndex(t => t.Name)
+            .IsUnique();
+        }
+
+    }
+}
